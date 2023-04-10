f5e5387600b129f6d60e88343b34cd03cd3fd623
krgarav <krgarav@gmail.com>
2023-04-10 07:32:44 +0530

    Add movie section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -8,21 +9,25 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMovieHandler = useCallback(async () => {
-    setError(null);
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
+    setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch(
+        "https://movie-app-8b082-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
-        throw new Error("Something went wrong ... Retrying");
+        throw new Error("Something went wrong!");
       }
+
       const data = await response.json();
-      const transformedMovies = await data.results.map((movieData) => {
+
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_data,
+          releaseDate: movieData.release_date,
         };
       });
       setMovies(transformedMovies);
@@ -30,46 +35,26 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  },[]);
+  }, []);
+
   useEffect(() => {
-    fetchMovieHandler();
-  }, [fetchMovieHandler]);
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
-  let timeOut;
-  const retryHandler = () => {
-    timeOut = setTimeout(() => {
-      return fetchMovieHandler();
-    }, 5000);
-  };
-  const cancelHandler = () => {
-    clearTimeout(timeOut);
-  };
-  let content = <p>No movies found.</p>;
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
+
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
+
   if (error) {
-    content = (
-      <p>
-        {error}
-        <br /> <button onClick={retryHandler}>Retry</button>
-        <button onClick={cancelHandler}>Cancel</button>
-      </p>
-    );
+    content = <p>{error}</p>;
   }
+
   if (isLoading) {
     content = <p>Loading...</p>;
   }
@@ -77,7 +62,10 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
     </React.Fragment>
